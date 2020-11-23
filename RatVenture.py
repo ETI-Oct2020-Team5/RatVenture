4310833d460cd0868df50a978a2b5181a4cd7b47
Zheng Feng <zheng_feng01@hotmail.com>
2020-11-23 15:48:38 +0800

    added town menu

--- RatVenture.py
+++ RatVenture.py
@@ -1,22 +1,51 @@
-def menu():
-    print("Welcome to RatVenture")
-    print("-----------------------")
+import sys
+
+def mainMenu():
+    print("Welcome to RatVenture!")
+    print("----------------------")
     print("[1] New Game")
     print("[2] Resume")
     print("[3] Exit")
 
-menu()
+def townMenu():
+    print("Day 1: You are in town. \n"
+            "[1] View Character \n" 
+            "[2] View Map \n"
+            "[3] Move \n"
+            "[4] Rest \n"
+            "[5] Save Game \n"
+            "[6] Exit Game \n\n")
+
+def heroStats():
+    print("The Hero \n"
+            "Damage: 2-4 \n"
+            "Defence:1 \n"
+            "HP:20")
+
+mainMenu()
 option = int(input("Enter your option: "))
 
 while option != 0:
     if option == 1:
-        print("New game function")
+        townMenu()
+        option = int(input("Enter your option: "))   
+
+        while option != 0: 
+            if option == 1: 
+                heroStats()
+                option = int(input("Enter your option: "))   
+                break
+
+            elif option == 2: 
+                print('map')
+                break
         break
     elif option == 2:
         print("Resume game function")
         break
-    elif option ==3:
+    elif option == 3:
         print("Thank you for playing, goodbye")
+        sys.exit()
         break
     else:
         menu()
