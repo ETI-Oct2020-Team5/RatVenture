aa68d198e72dcb639f9e9d23e07c1ac8b7b837ea
tankwangwei <49896524+tankwangwei@users.noreply.github.com>
2020-12-15 15:03:24 +0800

    Update test_ratventure_functions.py

--- test_ratventure_functions.py
+++ test_ratventure_functions.py
@@ -1,6 +1,12 @@
 import pytest
 from RatVenture import *
 
-def test_view_character():
-    character = viewCharacter()
-    assert character == viewCharacter()
+def test_view_stats():
+   """This is to test if the Hero Stats gets displayed and if the statistics is correct, dynamic and can respond to changes when the values change"""
+   value = townMenu_selection1()
+   assert value == player.name + "\nDamage: {}\nDefence: {}\nHP: {}".format(player.damage, player.defence, player.hp)
+
+def test_rest_function():
+   """This is to test if rest function is working. By calling this function, health will be 20 and day will be day + 1. Nothing is required for input"""
+   value = rest()
+   assert value == (20, player.day-1, player.day)
