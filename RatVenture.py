1696557c0f0a7225b0ca6994f7eb5ed3ee7c52c4
Liang Loong Lennon Wee <s10177333@connect.np.edu.sg>
2020-12-30 17:16:40 +0800

    documentation of savegame, resumegame, exitgame features

--- RatVenture.py
+++ RatVenture.py
@@ -44,13 +44,13 @@ player = Player()
 #--------Saved Player Class --------#
 class SavedPlayer:  # Player starts game with these stats
     def __init__(self):   # Defining parameters
-        self.name = 'SavedData'
-        self.damage = 'SavedData'
+        self.name = 'SavedData' #Default value of SavedData, will change when savegame function is called
+        self.damage = 'SavedData' #Default value of SavedData, will change when savegame function is called
         self.minDamage = 2
         self.maxDamage = 4
-        self.defence = 'SavedData'
-        self.hp = 'SavedData'
-        self.day = 'SavedData'
+        self.defence = 'SavedData' #Default value of SavedData, will change when savegame function is called
+        self.hp = 'SavedData' #Default value of SavedData, will change when savegame function is called
+        self.day = 'SavedData' #Default value of SavedData, will change when savegame function is called
         self.positionX = 0
         self.positionY = 0
         self.location = 'You are in a Town'
@@ -64,13 +64,13 @@ savedplayer = SavedPlayer()
 #--------Resumed Player Class --------#
 class ResumePlayer:  # Player starts game with these stats
     def __init__(self):   # Defining parameters
-        self.name = 'ResumeData'
-        self.damage = 'ResumeData'
+        self.name = 'ResumeData' #Default value of ResumeData, will change when resumegame function is called
+        self.damage = 'ResumeData' #Default value of ResumeData, will change when resumegame function is called
         self.minDamage = 2
         self.maxDamage = 4
-        self.defence = 'ResumeData'
-        self.hp = 'ResumeData'
-        self.day = 'ResumeData'
+        self.defence = 'ResumeData' #Default value of ResumeData, will change when resumegame function is called
+        self.hp = 'ResumeData' #Default value of ResumeData, will change when resumegame function is called
+        self.day = 'ResumeData' #Default value of ResumeData, will change when resumegame function is called
         self.positionX = 0
         self.positionY = 0
         self.location = 'You are in a Town'
@@ -96,99 +96,106 @@ rat = Rat()
 
 #-------Save Game-------#
 def savegame():
-    savedplayer.name = player.name
-    savedplayer.damage = player.damage
-    savedplayer.defence = player.defence
-    savedplayer.hp = player.hp
-    savedplayer.day = player.day
+    savedplayer.name = player.name #Assign savedplayer.name with player.name for saving feature
+    savedplayer.damage = player.damage #Assign savedplayer.damage with player.damage for saving feature
+    savedplayer.defence = player.defence #Assign savedplayer.defence with player.defence for saving feature
+    savedplayer.hp = player.hp #Assign savedplayer.hp with player.hp for saving feature
+    savedplayer.day = player.day #Assign savedplayer.day with player.day for saving feature
 
+    #dataList and headerList contains information in list to be written into csv to save and resume game.
     dataList=[savedplayer.name,savedplayer.damage,savedplayer.defence,savedplayer.hp, savedplayer.day, "Yes"]
+
+    #Last column adds the value "Yes" under the header of "Saved" in csv. This is for resumegame() function validation
     headerList=['Name','Damage','Defence','HP','Day', 'Saved']
 
+    #Reading saveddata.csv file if it exist and to write data into the csv file
     try:
         with open("saveddata.csv",'r') as infile:
-            reader = csv.reader(infile, delimiter=",")
+            reader = csv.reader(infile, delimiter=",") #Reading of file data
             header =next(reader)
-            for row in reader:
+            for row in reader: #Iterates through the row of data
                 playername=row[0]
                 playerdamage=row[1]
                 playerdefence=row[2]
                 playerhp=row[3]
                 if playername != null:
-                    
+                    #Opens saveddata.csv to write
                     with open('saveddata.csv','w',newline="") as csvfile:
                         writer=csv.writer(csvfile)
                         writer.writerow(headerList)
                         writer.writerow(dataList)
-                        csvfile.close()
-                        #print("Game saved successfully")
+                        csvfile.close() #Closes csv file
                         savedstats = savedplayer.name + "\nDamage: {}\nDefence: {}\nHP: {}\nDay: {}".format(savedplayer.damage, savedplayer.defence, savedplayer.hp, savedplayer.day)
-                        print("\nGame saved. Character stats: \n" + savedstats)
-                        #return savedstats
+                        print("\nGame saved. Character stats: \n" + savedstats) #Success message printed
+                        #Returns value for check with pytest
                         return (savedplayer.name, savedplayer.damage, savedplayer.defence, savedplayer.hp, savedplayer.day)
                         
-                    
+                #If csv is new and there is no header / data, this writes the initial header and data.    
                 elif playername == null:
-                    with open('saveddata.csv','w',newline="") as csvfile:
+                    with open('saveddata.csv','w',newline="") as csvfile: #Open csv file to write into
                         writer=csv.writer(csvfile)
                         writer.writerow(headerList)
                         writer.writerow(dataList)
-                        csvfile.close()
-                        #print("Game saved successfully")
+                        csvfile.close() #Close csv file
                         savedstats = savedplayer.name + "\nDamage: {}\nDefence: {}\nHP: {}\nDay: {}".format(savedplayer.damage, savedplayer.defence, savedplayer.hp, savedplayer.day)
+                        #Success message and stats printed
                         print("\nGame saved. Character stats: \n" + savedstats)
-                        #return savedstats
+                        #Value returned for check with pytest"
                         return (savedplayer.name, savedplayer.damage, savedplayer.defence, savedplayer.hp, savedplayer.day)
                 else:
                     pass
     except:
-        
-        with open('saveddata.csv','w',newline="") as csvfile:
+        #If there is no csv file created at all or is accidentally deleted, 
+        #this creates the csv file with the default player value written inside.
+        with open('saveddata.csv','w',newline="") as csvfile: #This opens csv file to write into
             writer=csv.writer(csvfile)
             writer.writerow(headerList)
             writer.writerow(dataList)
-            csvfile.close()
-            #print("Game saved successfully")
+            csvfile.close() #Closes csv file
             savedstats = savedplayer.name + "\nDamage: {}\nDefence: {}\nHP: {}\nDay: {}".format(savedplayer.damage, savedplayer.defence, savedplayer.hp, savedplayer.day)
+            #Print success message and display stats
             print("\nGame saved. Character stats: \n" + savedstats)
-            #return savedstats
+            #Return values for pytest checks
             return (savedplayer.name, savedplayer.damage, savedplayer.defence, savedplayer.hp, savedplayer.day)
 
-
+#---------Resume Game Feature---------#
 def resumegame():
+    #Attempt to read saveddata.csv file, if succeed, it will carry out the codes below
     try:
         with open("saveddata.csv",'r') as infile:
-                reader = csv.reader(infile, delimiter=",")
+                reader = csv.reader(infile, delimiter=",") #Reading of csv file data
                 header =next(reader)
-                for row in reader:          
-                    if row[5] != "Yes":
-                        print("No saved data found, creating new one instead")
-                        dataList=["The Hero", "2-4", "1", "20","1","No"]
-                        headerList=['Name','Damage','Defence','HP','Day','Saved']
-                        with open('saveddata.csv','w',newline="") as csvfile:
-                            writer=csv.writer(csvfile)
-                            writer.writerow(headerList)
-                            writer.writerow(dataList)
-                            csvfile.close()
-                        resumeplayer.name == row[0]
-                        resumeplayer.damage ==row[1]
-                        resumeplayer.defence==row[2]
-                        resumeplayer.hp==row[3]
-                        resumeplayer.day==row[4]
+                for row in reader: #Iterate through rows          
+                    if row[5] != "Yes": #Check if save game status in csv file is not "Yes"
+                        print("No saved data found, creating new one instead") #Error message shown
+                        dataList=["The Hero", "2-4", "1", "20","1","No"] #Default player value dataList to write into csv
+                        headerList=['Name','Damage','Defence','HP','Day','Saved'] #Default headers to write into csv
+                        with open('saveddata.csv','w',newline="") as csvfile: #Open csv file to write into
+                            writer=csv.writer(csvfile) 
+                            writer.writerow(headerList) #Writing Data
+                            writer.writerow(dataList) #Writing Data
+                            csvfile.close() #Close csv file
+                        resumeplayer.name == row[0] #Assign value to variable for resumeplayer
+                        resumeplayer.damage ==row[1] #Assign value to variable for resumeplayer
+                        resumeplayer.defence==row[2] #Assign value to variable for resumeplayer
+                        resumeplayer.hp==row[3] #Assign value to variable for resumeplayer
+                        resumeplayer.day==row[4] #Assign value to variable for resumeplayer
+                        #Return value of resumeplayer for pytest checks
                         return(resumeplayer.name,resumeplayer.damage,resumeplayer.defence,resumeplayer.hp,resumeplayer.day)
                     else:
+                        ##Return value of resumeplayer for pytest checks
                         return(resumeplayer.name,resumeplayer.damage,resumeplayer.defence,resumeplayer.hp,resumeplayer.day)
-                        print("Saved data found, resuming game")
-                        break
-    except:
+                        print("Saved data found, resuming game") #Success message
+                        break #Break out of loop
+    except: #If saveddata.csv is not found, this creates the file with the default player value and header
         dataList=["The Hero", "2-4", "1", "20","1","No"]
         headerList=['Name','Damage','Defence','HP','Day','Saved']
-        with open('saveddata.csv','w',newline="") as csvfile:
+        with open('saveddata.csv','w',newline="") as csvfile: #Create csv called saveddata.csv and writes dataList and headerList
             writer=csv.writer(csvfile)
             writer.writerow(headerList)
             writer.writerow(dataList)
-            csvfile.close()
-        print("No saved data file found, creating one now")
+            csvfile.close() #Close csv
+        print("No saved data file found, creating one now") #Print alternate success message
         
                         
     
@@ -196,15 +203,16 @@ def resumegame():
 
 
 
-
+#-------Exit game feature-------#
+#This feature is used to reset all values to the original default value if the programme is closed completely
 def exitgame():
-    dataList=["The Hero", "2-4", "1", "20","1","No"]
-    headerList=['Name','Damage','Defence','HP','Day','Saved']
-    with open('saveddata.csv','w',newline="") as csvfile:
+    dataList=["The Hero", "2-4", "1", "20","1","No"] #Default player value
+    headerList=['Name','Damage','Defence','HP','Day','Saved'] #Default header for csv
+    with open('saveddata.csv','w',newline="") as csvfile: #Opens / creates (if file does not exist or has been deleted) saveddata.csv.
             writer=csv.writer(csvfile)
-            writer.writerow(headerList)
-            writer.writerow(dataList)
-            csvfile.close()
+            writer.writerow(headerList) #Write headerList into csv
+            writer.writerow(dataList) #Write dataList into csv
+            csvfile.close() #Close csv
     
     sys.exit()
 
