eb836c312bf789d1ab5380abdda73ebdf51cc099
Zheng Feng <zheng_feng01@hotmail.com>
2021-01-31 13:14:07 +0800

    feat: added working codes to run() function

--- RatVenture.py
+++ RatVenture.py
@@ -372,8 +372,10 @@ def savegame():
                  
 #-------- Run feature --------#
 def run():
-    return
-
+    print() # New Line
+    print('You run and hide.')
+    rat.hp = 8 # Restore the Rat's HP to full when player runs away
+    openMenu() # To display the Open Menu 
 
 #------------------------ END OF NEW CODES  ------------------------# 
 
